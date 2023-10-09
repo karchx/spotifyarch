e3526de3d1cc752a0da6b91918f737327453dea6
Стив <kenethra.74@gmail.com>
2023-10-09 14:13:03 -0600

    feat: create config file

---
+++ src/config.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserConfig {
+    pub profile: Profile,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Profile {
+    pub client_id: String,
+    pub client_secret: String,
+}
+
+impl Profile {
+    fn new() -> Self {
+        Profile {
+            client_id: "".to_string(),
+            client_secret: "".to_string(),
+        }
+    }
+}
+
+impl UserConfig {
+    pub fn new() -> Self {
+        UserConfig {
+            profile: Profile::new(),
+        }
+    }
+    pub fn client_id(mut self, client_id: String) -> Self {
+        self.profile.client_id = client_id;
+        self
+    }
+    pub fn client_secret(mut self, client_secret: String) -> Self {
+        self.profile.client_secret = client_secret;
+        self
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate dirs;
 extern crate rpassword;
+extern crate toml;
 
 use std::fs;
 
 use anyhow::Result;
+use spotifyarch::config::UserConfig;
 
 fn init_spoterm_config_if_needed() -> Result<()> {
     let config_dir = dirs::home_dir()
@@ -19,8 +21,11 @@ fn init_spoterm_config_if_needed() -> Result<()> {
         println!("config.toml not found and input your <CLIENT_ID> and <CLIENT_SECRET>");
         let client_id = rpassword::read_password_from_tty(Some("Client ID: "))?;
         let client_secret = rpassword::read_password_from_tty(Some("Client Secret: "))?;
-        println!("ID: {}", client_id);
-        println!("SECRET: {}", client_secret);
+        let user_config = UserConfig::new()
+            .client_id(client_id)
+            .client_secret(client_secret);
+
+        fs::write(config.as_path(), toml::to_string(&user_config)?)?;
     }
     Ok(())
 }
