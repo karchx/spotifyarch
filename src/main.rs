2558ab1123668d8d31bd68b1e056241522360d8f
Стив <kenethra.74@gmail.com>
2023-10-09 15:45:57 -0600

    feat: get spotify config

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 extern crate dirs;
 extern crate rpassword;
+extern crate rspotify;
 extern crate toml;
 
 use std::fs;
 
 use anyhow::Result;
 use spotifyarch::config::UserConfig;
+
+// https://developer.spotify.com/documentation/general/guides/scopes/
+pub const SCOPES: [&str; 1] = [
+    // playlists
+    "playlist-read-private",
+];
 
 fn init_spoterm_config_if_needed() -> Result<()> {
     let config_dir = dirs::home_dir()
@@ -30,6 +37,41 @@ fn init_spoterm_config_if_needed() -> Result<()> {
     Ok(())
 }
 
-fn main() {
+fn get_spotify_client_id_and_secret() -> Result<(String, String), Box<dyn std::error::Error>> {
+    let config = dirs::home_dir()
+        .expect("can not find home directory")
+        .join(".spotifyarch")
+        .join("config.toml");
+    let config_content = fs::read_to_string(config.to_str().expect("can not read config file"))?;
+    let user_config: UserConfig = toml::from_str(&config_content)?;
+
+    Ok((
+        user_config.profile.client_id,
+        user_config.profile.client_secret,
+    ))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
     init_spoterm_config_if_needed().unwrap();
+
+    let (client_id, client_secret) = get_spotify_client_id_and_secret().unwrap();
+
+    let spotifyarch_cache = dirs::home_dir()
+        .expect("can not find home directory")
+        .join(".spotifyarch")
+        .join(".spotify_token_cache.json");
+    let mut oauth = rspotify::oauth2::SpotifyOAuth::default()
+        .scope(&SCOPES.join(" "))
+        .client_id(&client_id)
+        .client_secret(&client_secret)
+        .redirect_uri("http://localhost:8888/callback")
+        .cache_path(spotifyarch_cache)
+        .build();
+
+    let token_info = rspotify::util::get_token(&mut oauth).await.unwrap();
+
+    println!("{:?}", token_info);
+
+    Ok(())
 }
