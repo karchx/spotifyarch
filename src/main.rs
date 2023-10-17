09cffef74efd58da5e3b194a01a484027e1481f4
Стив <kenethra.74@gmail.com>
2023-10-16 21:53:37 -0600

    feat: init spotify

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,15 @@ mod token;
 
 use anyhow::Result;
 
+async fn init_spotify(
+    client_pub: &flume::Sender<event::ClientRequest>,
+    _client: &client::Client,
+    _state: &state::SharedState,
+) -> Result<()> {
+    client_pub.send(event::ClientRequest::GetUserPlaylists)?;
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn start_app(state: state::SharedState) -> Result<()> {
@@ -16,8 +25,17 @@ async fn start_app(state: state::SharedState) -> Result<()> {
     let auth_config = auth::AuthConfig::new(&state)?;
     let session = auth::new_session(&auth_config, true).await?;
     // create a spotify API client
-    let client = client::Client::new(session, auth_config, state.app_config.client_id.clone(), client_pub.clone());
+    let client = client::Client::new(
+        session,
+        auth_config,
+        state.app_config.client_id.clone(),
+        client_pub.clone(),
+    );
     client.init_token().await?;
+
+    init_spotify(&client_pub, &client, &state)
+        .await
+        .expect("Failed to initialize the Spotify data");
 
     Ok(())
 }
@@ -27,8 +45,9 @@ fn main() -> Result<()> {
     let cache_folder: std::path::PathBuf = config::get_cache_folder_path()?;
 
     let state = std::sync::Arc::new(state::State::new(&config_folder, &cache_folder)?);
-    
 
-    start_app(state);
+    if let Err(err) = start_app(state) {
+        tracing::error!("Encountered an error when running the application: {err:#}");
+    }
     Ok(())
 }
